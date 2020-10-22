f6df10494965c9206cbb6248c2cb4ce38be7044a
martreyz <martreyz@gmail.com>
2020-10-22 18:11:34 +0200

    Add function to paint results in page-js

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,29 @@
 
 const searchInput = document.querySelector(".js-inputSearch");
 const searchButton = document.querySelector(".js-button");
+const resultsList = document.querySelector(".js-resultsList");
 
+let seriesTitles;
+let seriesImages;
 function getSeries() {
+  resultsList.innerHTML = "";
   const searchInputValue = searchInput.value;
   fetch(`http://api.tvmaze.com/search/shows?q=${searchInputValue}`)
     .then((results) => results.json())
     .then((data) => {
       for (const serie of data) {
-        const seriesTitles = serie.show.name;
-        const seriesImages = serie.show.image;
-        console.log(seriesTitles);
+        seriesTitles = serie.show.name;
+        if (serie.show.image !== null) {
+          seriesImages = serie.show.image.original;
+        }
+        console.log(seriesImages);
+        paintResults();
       }
     });
 }
 
-searchInput.addEventListener("keyup", getSeries);
+searchButton.addEventListener("click", getSeries);
+
+function paintResults() {
+  resultsList.innerHTML += `<li><h2>${seriesTitles}</h2><img src="${seriesImages}" alt="${seriesTitles}" title="${seriesTitles}" class="main__result-pic"></li>`;
+}
