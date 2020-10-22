a836df439d8b76841ee993347cd74e21203aa003
martreyz <martreyz@gmail.com>
2020-10-22 19:28:55 +0200

    Add function to select favourite series and save in localStorage -js

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const searchInput = document.querySelector(".js-inputSearch");
 const searchButton = document.querySelector(".js-button");
 const resultsList = document.querySelector(".js-resultsList");
+const favourites = document.querySelector(".js-favourites");
 
 let seriesTitles;
 let seriesImages;
@@ -19,7 +20,6 @@ function getSeries() {
         } else {
           seriesImages = "../assets/images/imagealt.png";
         }
-        console.log(seriesImages);
         paintResults();
       }
     });
@@ -35,12 +35,14 @@ function paintResults() {
   resultsList.appendChild(serieListElement);
   serieListElement.appendChild(serieTitleElement);
   serieListElement.appendChild(serieImageElement);
+  serieListElement.classList.add("js-result-item");
   serieImageElement.src = seriesImages;
   serieImageElement.title = seriesTitles;
   serieImageElement.alt = seriesTitles;
   serieImageElement.classList.add("main__result-pic");
   serieTitleElement.appendChild(serieTitleContent);
 
+  listenResults();
   // resultsList.innerHTML += `<li><h2>${seriesTitles}</h2><img src="${seriesImages}" alt="${seriesTitles}" title="${seriesTitles}" class="main__result-pic"></li>`;
 }
 
@@ -53,3 +55,26 @@ function changeEnterAction(event) {
 }
 
 searchInput.addEventListener("keydown", changeEnterAction);
+
+//Favourites
+
+const favouriteSeries = [];
+
+function addToFavourites(event) {
+  let favouriteSerie = event.currentTarget;
+  favouriteSerie.classList.toggle("selected");
+  if (favouriteSeries.indexOf(favouriteSerie.innerHTML) !== -1) {
+    let serieIndex = favouriteSeries.indexOf(favouriteSerie.innerHTML);
+    favouriteSeries.splice(serieIndex, 1);
+  } else {
+    favouriteSeries.push(favouriteSerie.innerHTML);
+  }
+  localStorage.setItem("favourite", JSON.stringify(favouriteSeries));
+}
+
+function listenResults() {
+  const resultsListItems = document.querySelectorAll(".js-result-item");
+  for (const resultsItem of resultsListItems) {
+    resultsItem.addEventListener("click", addToFavourites);
+  }
+}
