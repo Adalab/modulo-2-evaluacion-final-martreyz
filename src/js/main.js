281f491c7f356e5f4d10cf83e5983981e26fb62b
martreyz <martreyz@gmail.com>
2020-10-22 17:38:29 +0200

    Add fetch function to get series from API -js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const searchInput = document.querySelector(".js-inputSearch");
+const searchButton = document.querySelector(".js-button");
+
+function getSeries() {
+  const searchInputValue = searchInput.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${searchInputValue}`)
+    .then((results) => results.json())
+    .then((data) => {
+      for (const serie of data) {
+        const seriesTitles = serie.show.name;
+        const seriesImages = serie.show.image;
+        console.log(seriesTitles);
+      }
+    });
+}
+
+searchInput.addEventListener("keyup", getSeries);
